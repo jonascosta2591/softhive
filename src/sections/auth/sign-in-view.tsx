6cfdac5c3e79b2809b07d04302569031c258e5b7
Jonas Lima <117112836+jonascosta2591@users.noreply.github.com>
2025-08-25 17:36:20 -0300

    Correcao de pequenos bugs

--- src/sections/auth/sign-in-view.tsx
+++ src/sections/auth/sign-in-view.tsx
@@ -146,7 +146,7 @@ export function SignInView() {
         autoComplete="email"
       />
 
-      <Link variant="body2" color="inherit" sx={{ mb: 1.5 }}>
+      <Link variant="body2" color="inherit" sx={{ mb: 1.5 }} href="./esqueci-senha">
         Esqueceu sua senha?
       </Link>
 
